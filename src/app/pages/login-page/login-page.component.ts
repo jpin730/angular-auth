78cfe8f3626d654ceb857ce991155bb14b5be712
Jaime Pineda <jpin730@pm.me>
2024-02-26 15:46:25 -0600

    feat: :lipstick: add login and register templates

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { PATH } from '../../constants/path.constant'
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './login-page.component.html',
 })
-export default class LoginPageComponent {}
+export default class LoginPageComponent {
+  PATH = PATH
+}
--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { PATH } from '../../constants/path.constant'
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './register-page.component.html',
 })
-export default class RegisterPageComponent {}
+export default class RegisterPageComponent {
+  PATH = PATH
+}
