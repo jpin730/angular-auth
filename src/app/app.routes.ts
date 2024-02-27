b25df8b34c3e8110bd007a9d5329564261694daf
Jaime Pineda <jpin730@pm.me>
2024-02-27 12:18:51 -0600

    feat: :lipstick: add app container component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,29 @@
 import { Routes } from '@angular/router'
+import { ContainerComponent } from './components/container/container.component'
 import { PATH } from './constants/path.constant'
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: PATH.HOME,
-    pathMatch: 'full',
-  },
-  {
-    path: PATH.HOME,
-    loadComponent: () => import('./pages/home-page/home-page.component'),
-  },
-  {
-    path: PATH.LOGIN,
-    loadComponent: () => import('./pages/login-page/login-page.component'),
-  },
-  {
-    path: PATH.REGISTER,
-    loadComponent: () =>
-      import('./pages/register-page/register-page.component'),
-  },
-  {
-    path: '**',
-    redirectTo: PATH.HOME,
+    component: ContainerComponent,
+    children: [
+      {
+        path: PATH.HOME,
+        loadComponent: () => import('./pages/home-page/home-page.component'),
+      },
+      {
+        path: PATH.LOGIN,
+        loadComponent: () => import('./pages/login-page/login-page.component'),
+      },
+      {
+        path: PATH.REGISTER,
+        loadComponent: () =>
+          import('./pages/register-page/register-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: PATH.HOME,
+      },
+    ],
   },
 ]
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './container.component.html',
+})
+export class ContainerComponent {}
