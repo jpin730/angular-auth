e6e5032ae13828fd93e25e9761b18c9f446f4878
Jaime Pineda <jpin730@pm.me>
2024-02-27 00:11:51 -0600

    feat: :sparkles: add reactive forms to login and reactive pages

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 import { PATH } from '../../constants/path.constant'
+import { ControlErrorsDirective } from '../../directives/control-errors.directive'
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, ControlErrorsDirective],
   templateUrl: './login-page.component.html',
 })
 export default class LoginPageComponent {
+  private readonly fb = inject(FormBuilder)
+
   PATH = PATH
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  })
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched()
+      return
+    }
+  }
 }
--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 import { PATH } from '../../constants/path.constant'
+import { ControlErrorsDirective } from '../../directives/control-errors.directive'
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, ControlErrorsDirective],
   templateUrl: './register-page.component.html',
 })
 export default class RegisterPageComponent {
+  private readonly fb = inject(FormBuilder)
+
   PATH = PATH
+
+  registerForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched()
+      return
+    }
+  }
 }
