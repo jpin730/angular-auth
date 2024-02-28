612954c25f803f09253c18104e7eca2605b39373
Jaime Pineda <jpin730@pm.me>
2024-02-28 15:17:47 -0600

    refactor: :lipstick: refactor app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { fadeAnimation } from './animations/fade.animation'
+import { LoaderComponent } from './components/loader/loader.component'
+import { NotificationComponent } from './components/notification/notification.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet />',
+  imports: [RouterOutlet, LoaderComponent, NotificationComponent],
+  template: `
+    <div [@fade]="outlet.activatedRouteData">
+      <router-outlet #outlet="outlet" />
+    </div>
+    <app-loader />
+    <app-notification />
+  `,
+  animations: [fadeAnimation()],
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,27 @@
 import { Routes } from '@angular/router'
-import { ContainerComponent } from './components/container/container.component'
 import { PATH } from './constants/path.constant'
 import { isAuthenticatedGuard } from './guards/is-authenticated.guard'
 import { isNotAuthenticatedGuard } from './guards/is-not-authenticated.guard'
 
 export const routes: Routes = [
   {
-    path: '',
-    component: ContainerComponent,
-    children: [
-      {
-        path: PATH.HOME,
-        loadComponent: () => import('./pages/home-page/home-page.component'),
-        canActivate: [isAuthenticatedGuard],
-      },
-      {
-        path: PATH.LOGIN,
-        loadComponent: () => import('./pages/login-page/login-page.component'),
-        canActivate: [isNotAuthenticatedGuard],
-      },
-      {
-        path: PATH.REGISTER,
-        loadComponent: () =>
-          import('./pages/register-page/register-page.component'),
-        canActivate: [isNotAuthenticatedGuard],
-      },
-      {
-        path: '**',
-        redirectTo: PATH.HOME,
-      },
-    ],
+    path: PATH.HOME,
+    loadComponent: () => import('./pages/home-page/home-page.component'),
+    canActivate: [isAuthenticatedGuard],
+  },
+  {
+    path: PATH.LOGIN,
+    loadComponent: () => import('./pages/login-page/login-page.component'),
+    canActivate: [isNotAuthenticatedGuard],
+  },
+  {
+    path: PATH.REGISTER,
+    loadComponent: () =>
+      import('./pages/register-page/register-page.component'),
+    canActivate: [isNotAuthenticatedGuard],
+  },
+  {
+    path: '**',
+    redirectTo: PATH.HOME,
   },
 ]
