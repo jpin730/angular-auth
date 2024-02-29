03a7f19f4c035221382a32b592a23c5034c7f130
Jaime Pineda <jpin730@pm.me>
2024-02-29 15:47:13 -0600

    feat: :lipstick: add user list component

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core'
+import { AsyncPipe } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { RolePipe } from '../../pipes/role.pipe'
+import { UsersService } from '../../services/users.service'
+import { UserStatusComponent } from '../user-status/user-status.component'
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
+  imports: [AsyncPipe, RolePipe, UserStatusComponent],
   templateUrl: './user-list.component.html',
 })
-export class UserListComponent {}
+export class UserListComponent {
+  private readonly usersService = inject(UsersService)
+
+  users$ = this.usersService.getAllUsers()
+}
