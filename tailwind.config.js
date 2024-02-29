890bdbd3b0b3a2477995925afc8435c34880231d
Jaime Pineda <jpin730@pm.me>
2024-02-29 12:11:44 -0600

    refactor: :lipstick: refactor with global color extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-dark': '#111827',
+        'gray-light-hover': '#4b5563',
+        'gray-light': '#6b7280',
+        'indigo-hover': '#6366f1',
+        indigo: '#4f46e5',
+        red: '#e11d48',
+        'white-hover': '#f9fafb',
+      },
+    },
   },
   plugins: [],
 }
