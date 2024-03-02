1bf52f2444e882234a51263bc3341b8bb9d24cbf
Jaime Pineda <jpin730@pm.me>
2024-03-01 21:49:12 -0600

    refactor: :recycle: add users signal in users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
-import { Injectable, inject } from '@angular/core'
-import { Observable, catchError, finalize } from 'rxjs'
+import { Injectable, computed, inject, signal } from '@angular/core'
+import { Observable, catchError, finalize, tap } from 'rxjs'
 import { environment } from '../../environments/environment'
 import { API_ENDPOINTS } from '../constants/api-endpoints.constant'
 import { User } from '../interfaces/user.interface'
@@ -17,10 +17,17 @@ export class UsersService {
 
   private readonly baseUrl = `${environment.apiBaseUrl}${API_ENDPOINTS.USERS}`
 
+  private _users = signal<User[]>([])
+
+  users = computed(() => this._users())
+
   getAllUsers(): Observable<User[]> {
     this.loader.show()
     this.notification.hide()
     return this.http.get<User[]>(`${this.baseUrl}`).pipe(
+      tap((users) => {
+        this._users.set(users)
+      }),
       finalize(() => {
         this.loader.hide()
       }),
